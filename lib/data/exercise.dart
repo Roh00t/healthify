4af756869e53ae81f0ac96a8b97e605efdfa6f75
Roh00t <pandarohit05@gmail.com>
2022-02-02 01:01:39 +0800

    added more exercises

--- lib/data/exercise.dart
+++ lib/data/exercise.dart
@@ -10,37 +10,37 @@ final exercises1 = [
       name: 'Push Up',
       duration: Duration(seconds: 40),
       noOfReps: 10,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/pushups.mp4'),
   Exercise(
       name: 'Plank',
       duration: Duration(seconds: 20),
       noOfReps: 15,
-      videoUrl: 'assets/workouts/yoga2.mp4'),
+      videoUrl: 'assets/workouts/planks.mp4'),
   Exercise(
       name: 'Pull Up',
       duration: Duration(seconds: 50),
       noOfReps: 8,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/pullups.mp4'),
   Exercise(
       name: 'Crunch',
       duration: Duration(seconds: 10),
       noOfReps: 12,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/crunches.mp4'),
   Exercise(
       name: 'Lunges',
       duration: Duration(seconds: 20),
       noOfReps: 15,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/lunges.mp4'),
   Exercise(
       name: 'Squats',
       duration: Duration(seconds: 30),
       noOfReps: 20,
-      videoUrl: 'assets/workouts/yoga2.mp4'),
+      videoUrl: 'assets/workouts/squats.mp4'),
   Exercise(
-      name: 'Burpees',
+      name: 'Yoga',
       duration: Duration(seconds: 30),
       noOfReps: 10,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/yoga2.mp4'),
 ];
 
 final exercises2 = [
@@ -48,37 +48,37 @@ final exercises2 = [
       name: 'Push Up',
       duration: Duration(seconds: 40),
       noOfReps: 6,
-      videoUrl: 'assets/workouts/yoga2.mp4'),
+      videoUrl: 'assets/workouts/pushups.mp4'),
   Exercise(
-      name: 'Side Jump',
+      name: 'Skipping',
       duration: Duration(seconds: 30),
       noOfReps: 6,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/skipping.mp4'),
   Exercise(
       name: 'Plank',
       duration: Duration(seconds: 20),
       noOfReps: 8,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/planks.mp4'),
   Exercise(
       name: 'Crunch',
       duration: Duration(seconds: 25),
       noOfReps: 10,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/crunches.mp4'),
   Exercise(
       name: 'Lunges',
       duration: Duration(seconds: 35),
       noOfReps: 12,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/lunges.mp4'),
   Exercise(
       name: 'Squats',
       duration: Duration(seconds: 10),
       noOfReps: 5,
-      videoUrl: 'assets/workouts/yoga2.mp4'),
+      videoUrl: 'assets/workouts/squats.mp4'),
   Exercise(
-      name: 'Burpees',
+      name: 'Yoga',
       duration: Duration(seconds: 50),
       noOfReps: 15,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/yoga.mp4'),
 ];
 
 final exercises3 = [
@@ -86,24 +86,24 @@ final exercises3 = [
       name: 'Burpees',
       duration: Duration(seconds: 60),
       noOfReps: 20,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/burpees.mp4'),
   Exercise(
       name: 'Push Up',
       duration: Duration(seconds: 10),
       noOfReps: 5,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/pushups.mp4'),
   Exercise(
-      name: 'Side Jump',
+      name: 'Skipping',
       duration: Duration(seconds: 20),
       noOfReps: 8,
-      videoUrl: 'assets/workouts/yoga2.mp4'),
+      videoUrl: 'assets/workouts/skipping.mp4'),
   Exercise(
       name: 'Plank',
       duration: Duration(seconds: 25),
       noOfReps: 1,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/planks.mp4'),
   Exercise(
-      name: 'Crunch',
+      name: 'Yoga',
       duration: Duration(seconds: 25),
       noOfReps: 12,
       videoUrl: 'assets/workouts/yoga2.mp4'),
@@ -111,9 +111,9 @@ final exercises3 = [
       name: 'Lunges',
       duration: Duration(seconds: 40),
       noOfReps: 16,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/lunges.mp4'),
   Exercise(
-      name: 'Squats',
+      name: 'Yoga',
       duration: Duration(seconds: 20),
       noOfReps: 14,
       videoUrl: 'assets/workouts/yoga.mp4'),
@@ -124,7 +124,7 @@ final exercises4 = [
       name: 'Lunges',
       duration: Duration(seconds: 40),
       noOfReps: 6,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/lunges.mp4'),
   Exercise(
       name: 'Burpees',
       duration: Duration(seconds: 10),
@@ -134,25 +134,25 @@ final exercises4 = [
       name: 'Push Up',
       duration: Duration(seconds: 20),
       noOfReps: 8,
-      videoUrl: 'assets/workouts/yoga2.mp4'),
+      videoUrl: 'assets/workouts/pushups.mp4'),
   Exercise(
-      name: 'Side Jump',
+      name: 'Skipping',
       duration: Duration(seconds: 25),
       noOfReps: 14,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/skipping.mp4'),
   Exercise(
       name: 'Plank',
       duration: Duration(seconds: 15),
       noOfReps: 12,
-      videoUrl: 'assets/workouts/yoga.mp4'),
+      videoUrl: 'assets/workouts/planks.mp4'),
   Exercise(
       name: 'Crunch',
       duration: Duration(seconds: 120),
       noOfReps: 20,
-      videoUrl: 'assets/workouts/yoga2.mp4'),
+      videoUrl: 'assets/workouts/crunches.mp4'),
   Exercise(
       name: 'Squats',
       duration: Duration(seconds: 30),
       noOfReps: 15,
-      videoUrl: 'assets/workouts/burpees.mp4'),
+      videoUrl: 'assets/workouts/squats.mp4'),
 ];
